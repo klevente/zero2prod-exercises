37be035786a2db3c0de909daf4f61e14c1c6c60f
Levente Krizsan <lewalevi@gmail.com>
2021-11-04 20:50:41 +0100

    refactor(project): split files into new structure

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use zero2prod::startup::run;
 
 // Launch our application in the background
 fn spawn_app() -> String {
@@ -6,7 +7,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // Retrieve the port assigned by the OS
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     // spawn a background thread that runs our server
     let _ = tokio::spawn(server);
     // Return the application address to the caller
