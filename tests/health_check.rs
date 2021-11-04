d3068525dccdb747592574049f4b97e22675fa14
Levente Krizsan <lewalevi@gmail.com>
2021-11-04 21:12:50 +0100

    test(subscriptions): add database asserts to subscriptions test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 // Launch our application in the background
@@ -38,6 +40,14 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+    // the `Connection` trait must be imported in order to call the `connect` method
+    // it also needs to be marked mutable
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
@@ -52,6 +62,15 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    // the type of `saved` is generated at compile time from the query, if it is valid
+    let saved = sqlx::query!("select email, name from subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]
