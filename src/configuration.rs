bc96ef187c0c84b37693597e05a78b27c8d9c63b
Levente Krizsan <lewalevi@gmail.com>
2021-11-05 15:02:51 +0100

    fix(config): remove unused import

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,5 @@
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::{PgConnectOptions, PgSslMode};
-use sqlx::ConnectOptions;
 use std::convert::{TryFrom, TryInto};
 
 #[derive(serde::Deserialize)]
