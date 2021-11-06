99ffa78792603bbb0ac19a2196fab69585816ac8
Levente Krizsan <lewalevi@gmail.com>
2021-11-06 17:50:09 +0100

    feat(subs): add migration for creating sub tokens table

---
+++ migrations/20211106164850_create_subscription_tokens_table.sql
@@ -0,0 +1,6 @@
+-- Create Subscription Tokens table
+create table subscription_tokens(
+    subscription_token text not null,
+    subscriber_id uuid not null references subscriptions (id),
+    primary key (subscription_token)
+);
