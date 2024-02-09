17a65875982bb8263ce48bb4049d85f0493b32f6
Levente Krizsán <lewalevi@gmail.com>
2024-02-09 23:41:47 +0100

    feat: implement admin dashboard

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,8 +1,10 @@
 use crate::{
     authentication::{validate_credentials, AuthError, Credentials},
     routes::error_chain_fmt,
+    session_state::TypedSession,
+    utils::see_other,
 };
-use actix_web::{error::InternalError, http::header::LOCATION, web, HttpResponse};
+use actix_web::{error::InternalError, web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::Secret;
 use sqlx::PgPool;
@@ -14,12 +16,13 @@ pub struct FormData {
 }
 
 #[tracing::instrument(
-    skip(form, pool),
+    skip(form, pool, session),
     fields(username = tracing::field::Empty, user_id = tracing::field::Empty)
 )]
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
+    session: TypedSession,
     // Use an `InternalError` to preserve the fact that an error occurred, while attaching
     // a custom error HTTP response to it which can be returned to the user.
 ) -> Result<HttpResponse, InternalError<LoginError>> {
@@ -33,27 +36,30 @@ pub async fn login(
     match validate_credentials(credentials, &pool).await {
         Ok(user_id) => {
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
-            Ok(HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/"))
-                .finish())
+
+            session.renew();
+            session
+                .insert_user_id(user_id)
+                .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
+
+            Ok(see_other("/admin/dashboard"))
         }
         Err(e) => {
             let e = match e {
                 AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
                 AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
             };
-
-            // This creates the flash message cookie and signs it, all in the background
-            FlashMessage::error(e.to_string()).send();
-
-            let response = HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/login"))
-                .finish();
-
-            // This propagates the underlying error `e` back to `actix`, so it can log it out properly
-            Err(InternalError::from_response(e, response))
+            Err(login_redirect(e))
         }
     }
+}
+
+fn login_redirect(e: LoginError) -> InternalError<LoginError> {
+    // This creates the flash message cookie and signs it, all in the background
+    FlashMessage::error(e.to_string()).send();
+    let response = see_other("/login");
+    // This propagates the underlying error `e` back to `actix`, so it can log it out properly
+    InternalError::from_response(e, response)
 }
 
 #[derive(thiserror::Error)]
