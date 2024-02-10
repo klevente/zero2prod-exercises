867adfe1d7717539be57c801364eb5a09058262f
Levente Krizsán <lewalevi@gmail.com>
2024-02-10 21:38:36 +0100

    refactor: remove basic auth

--- src/authentication/mod.rs
+++ src/authentication/mod.rs
@@ -2,6 +2,4 @@ mod middleware;
 mod password;
 
 pub use middleware::{reject_anonymous_users, UserId};
-pub use password::{
-    basic_authentication, change_password, validate_credentials, AuthError, Credentials,
-};
+pub use password::{change_password, validate_credentials, AuthError, Credentials};
